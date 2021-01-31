165e1e0126a4adfcf457d2ec989dc40077ab1c21
Jorge Luis Bou-saad <jbousing@gmail.com>
2021-01-31 01:44:33 -0400

    se organiza mejor la escena Game

--- static/js/game.js
+++ static/js/game.js
@@ -30,16 +30,20 @@ export class Game extends Phaser.Scene {
         });
     }
 
-    create(){
 
-    
-        //this.physics.world.setBoundsCollision(true,true,true,true); //NO CHOCA CON LOS BORDES
+    createPlayer(){
 
-        this.add.image(400,250,"background");
-        this.gameoverImage = this.add.image(400,90,"gameover");
-        this.gameoverImage.visible = false;
+        this.player = this.physics.add.sprite(200,100,"player");
+        this.animationPlayer = this.anims.create({
+            key: "pj-walk",
+            frames: this.anims.generateFrameNumbers('player'),
+            frameRate: 10,
+            repeat: -1 //infinite loops
+        });
+    }
 
-        //this.cofre = this.physics.add.image(300,200,"cofre").setImmovable( );
+    createCofre(){
+
         this.cofre = this.physics.add.sprite(300,200,"cofre").setImmovable();
         this.animationCofre = this.anims.create({
             key: "cofre-idle",
@@ -49,9 +53,11 @@ export class Game extends Phaser.Scene {
         });
 
         this.cofre.anims.play("cofre-idle",true);
+    }
 
+    createFuego(){
 
-        this.cofre = this.physics.add.sprite(500,200,"fuego").setImmovable();
+        this.fuego = this.physics.add.sprite(500,200,"fuego").setImmovable();
         this.animationCofre = this.anims.create({
             key: "fuego",
             frames: this.anims.generateFrameNumbers('fuego'),
@@ -59,20 +65,10 @@ export class Game extends Phaser.Scene {
             repeat: -1 //infinite loops
         });
 
-        this.cofre.anims.play("fuego",true);
+        this.fuego.anims.play("fuego",true);
+    }
 
-
-
-       // this.player = this.physics.add.image(200,100,"player");
-       //this.player = this.add.sprite(200,100,"player");
-
-        this.player = this.physics.add.sprite(200,100,"player");
-        this.animationPlayer = this.anims.create({
-            key: "pj-walk",
-            frames: this.anims.generateFrameNumbers('player'),
-            frameRate: 10,
-            repeat: -1 //infinite loops
-        });
+    createEnemy(){
 
         this.enemy = this.physics.add.sprite(400,400,"enemy");
         this.animationPlayer = this.anims.create({
@@ -83,14 +79,30 @@ export class Game extends Phaser.Scene {
         });
 
         this.enemy.anims.play("lier-walk",true);
+    }
+
     
+    create(){
 
-        this.player.setCollideWorldBounds(true);
-        this.physics.add.collider(this.player,this.cofre);
-        this.physics.add.collider(this.player,this.enemy);
+        
+        this.add.image(400,250,"background");
+        this.gameoverImage = this.add.image(400,90,"gameover");
+        this.gameoverImage.visible = false;
 
-        this.cursors = this.input.keyboard.createCursorKeys();
+        //this.cofre = this.physics.add.image(300,200,"cofre").setImmovable( );
+       // this.player = this.physics.add.image(200,100,"player");
+       //this.player = this.add.sprite(200,100,"player");
 
+        
+       this.createPlayer();
+       this.createCofre();
+       this.createFuego();
+       this.createEnemy();
+       
+       this.player.setCollideWorldBounds(true);
+       this.physics.add.collider(this.player,this.cofre);
+       this.physics.add.collider(this.player,this.enemy);
+       this.cursors = this.input.keyboard.createCursorKeys();
         this.velocity = 100;
     }
 
