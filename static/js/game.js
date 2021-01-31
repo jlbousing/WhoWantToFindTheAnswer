275e4c27487ef4fade756f6332861f5eddf084c6
Jorge Luis Bou-saad <jbousing@gmail.com>
2021-01-30 22:10:45 -0400

    imagenes bases

--- static/js/game.js
+++ static/js/game.js
@@ -7,10 +7,20 @@ export class Game extends Phaser.Scene {
     preload(){
 
         this.load.image("background","images/terrain-test.png");
+        this.load.image("gameover","images/gameover-test.png");
+        this.load.image("cofre","images/cofre-test.png");
+        this.load.image("player","images/finder-pj-test-1.png");
 
     }
 
     create(){
-        this.add.image(0,0,"background");
+        this.add.image(400,250,"background");
+        this.gameoverImage = this.add.image(400,90,"gameover");
+        this.gameoverImage.visible = false;
+
+        this.cofreImage = this.physics.add.image(40,30,"cofre");
+
+        this.playerImage = this.physics.add.image(200,100,"player");
+
     }
 }
