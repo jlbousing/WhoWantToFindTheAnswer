28760345ee1f6a4b153638eaf79a0af573f2bd74
Jorge Luis Bou-saad <jbousing@gmail.com>
2021-01-31 02:00:00 -0400

    se agrega archivo que tiene preguntas y respuestas de prueba

---
+++ static/js/questions.js
@@ -0,0 +1,30 @@
+export const questions = [
+    {
+        question: "Cuanto es 2 + 2",
+        optionA: "5",
+        optionB: "3",
+        optionC: "4",
+        optionD: "1"
+    },
+    {
+        question: "¿ Cómo se llama el libertado de Venezuela ?",
+        optionA: "Francisco de Miranda",
+        optionB: "Hugo Chávez",
+        optionC: "Rómulo Betancourt",
+        optionD: "Simón Bolivar"
+    },
+    {
+        question: "¿ Cómo se llama el CEO de Tesla ?",
+        optionA: "Francisco de Miranda",
+        optionB: "Elon Musk",
+        optionC: "Rómulo Betancourt",
+        optionD: "Simón Bolivar"
+    },
+    {
+        question: "¿ Por qué el cielo es azul ?",
+        optionA: "Porque la luz del sol interactúa con la atmósfera",
+        optionB: "Porque lo pintaron de azul",
+        optionC: "Por el reflejo del mar",
+        optionD: "Por los gases invernaderos"
+    }
+];
