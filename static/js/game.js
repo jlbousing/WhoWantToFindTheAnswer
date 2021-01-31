8018e38c197bf7bdeabab3e02f4aae634a24c907
Jorge Luis Bou-saad <jbousing@gmail.com>
2021-01-30 23:57:11 -0400

    movimiento del player

--- static/js/game.js
+++ static/js/game.js
@@ -14,13 +14,42 @@ export class Game extends Phaser.Scene {
     }
 
     create(){
+
+    
+        //this.physics.world.setBoundsCollision(true,true,true,true); //NO CHOCA CON LOS BORDES
+
         this.add.image(400,250,"background");
         this.gameoverImage = this.add.image(400,90,"gameover");
         this.gameoverImage.visible = false;
 
-        this.cofreImage = this.physics.add.image(40,30,"cofre");
+        this.cofre = this.physics.add.image(100,30,"cofre").setImmovable( );
 
-        this.playerImage = this.physics.add.image(200,100,"player");
+        this.player = this.physics.add.image(200,100,"player");
+        this.player.setCollideWorldBounds(true);
+        
+        this.physics.add.collider(this.player,this.cofre);
 
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.velocity = 100;
+    }
+
+    update(){
+
+
+        if (this.cursors.left.isDown){
+            this.player.setVelocityX(this.velocity * -1);
+        }
+        else if (this.cursors.right.isDown){
+            this.player.setVelocityX(this.velocity);
+        }
+        else if (this.cursors.up.isDown){
+            this.player.setVelocityY(this.velocity * -1);
+        }
+        else if (this.cursors.down.isDown){
+            this.player.setVelocityY(this.velocity);
+        }else{
+            this.player.setVelocity(0,0);
+        }
     }
 }
